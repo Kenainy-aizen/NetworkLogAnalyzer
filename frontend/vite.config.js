a08e2051c56ed28b68f1cfb4b14c1cdbe5f54dc7
kenainy_aizen <kenainyravelomanana@gmail.com>
2026-07-05 00:05:43 +0300

    step twenty-two done — Docker + docker-compose (prêt à déployer)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000',
+      '/hubs': {
+        target: 'http://localhost:5000',
+        ws: true,
+      }
+    }
+  }
 })
